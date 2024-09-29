02d1ba5793a5a28406e4b1360e1369ba71c02d8f
Andrea Kim <80376288+aecone@users.noreply.github.com>
2024-09-28 22:32:08 -0400

    Create schema.sql

---
+++ schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE users(
+  user_id SERIAL INT PRIMARY KEY, 
+  username VARCHAR(100),
+  pw VARCHAR(100), 
+  subscribed 
+);
+
+CREATE TABLE insta_accounts(
+  insta_username VARCHAR(100),
+  
+);
