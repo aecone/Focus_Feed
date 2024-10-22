fa85ab9160428c27d72af1a4c25c45bc83eb5b0d
aneeshaa1 <acharyannie@gmail.com>
2024-10-22 17:52:00 -0400

    transfer over current frontend progress (html and css)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      {/* <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,6 +19,49 @@ function App() {
           Learn React
         </a>
       </header>
+    </div> */}
+
+    <div>
+      <body>
+          {/* contains the navbar */}
+          <header>
+              <div class="logo">FocusFeed</div>
+              <div class = "navbar">
+                  
+                  <nav>
+                      <ul>
+                          {/* list items for each section: Feed, Follow, and Profile */}
+                          <li><a href="#">Feed</a></li>
+                          <li><a href="#">Follow</a></li>
+                          <li><a href="#">Profile</a></li>
+                      </ul>
+                  </nav>
+              </div>
+          </header>
+
+          {/* search bar section below the nav bar */}
+          <div class = "search-bar">
+              {/* input field for search queries */}
+              <input type="text" placeholder="Search"/>
+          </div>
+
+          {/* grid container for the feed's content */}
+          <main>
+              <section class = "feed-grid">
+                  {/* placeholder card for each post */}
+                  <div class = "card"></div>
+                  <div class = "card"></div>
+                  <div class = "card"></div>
+                  <div class = "card"></div>
+                  <div class = "card"></div>
+                  <div class = "card"></div>
+                  <div class = "card"></div>
+                  <div class = "card"></div>
+              </section>
+          </main>
+      </body>
+      
+    </div>
     </div>
   );
 }
