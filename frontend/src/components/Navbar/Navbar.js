c616132daba73da25b316ff65f2833ee527b2964
anna belenko <66851299+annabelenko@users.noreply.github.com>
2024-10-28 16:43:26 -0400

    added logo

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './Navbar.css';
+import logo from '../../assets/images/logo.svg'; // Adjust path based on your setup
 
 function Navbar() {
   return (
     <header className="navbar-container">
-      <div className="logo">FocusFeed</div>
-      <nav className="navbar">
-        <ul>
+      <div className="logo-container">
+        <img src={logo} alt="FocusFeed Logo" className="logo" />
+      </div>
+      <nav>
+        <ul className="navbar-links">
           <li><a href="#">Feed</a></li>
           <li><a href="#">Follow</a></li>
           <li><a href="#">Profile</a></li>
