784210dbf3836ad834235fa3fb837337bfa6fbe1
anna belenko <66851299+annabelenko@users.noreply.github.com>
2024-11-13 23:25:43 -0500

    added sign up button

--- frontend/src/components/LoginPage/LoginPage.jsx
+++ frontend/src/components/LoginPage/LoginPage.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
-import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from '@clerk/clerk-react';
+import './LoginPage.css';
 
 const Login = () => {
   return (
     <div className="login-container">
       <h2>Welcome to Focus Feed</h2>
 
-      {/* Show SignInButton if the user is signed out */}
+      {/* Show SignInButton and SignUpButton if the user is signed out */}
       <SignedOut>
-        <p>Please sign in to continue:</p>
-        <SignInButton />
+        <p>Please sign in or register to continue:</p>
+        <div className="auth-buttons">
+          <SignInButton />
+          <SignUpButton />
+        </div>
       </SignedOut>
 
       {/* Show UserButton if the user is signed in */}
